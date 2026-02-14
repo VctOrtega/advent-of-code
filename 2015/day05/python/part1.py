63a3a134d03d8c444f22de18a8c66c42d53a06f4
victor <victorortega2ti@gmail.com>
2026-02-14 14:28:41 -0800

    Created day05 and day07 folders and files

---
+++ 2015/day05/python/part1.py
@@ -0,0 +1,11 @@
+# Advent of Code
+# Year: 2015
+# Day: 5
+
+def main():
+    with open('input.txt', 'r') as f:
+        for ch in f.read():
+            pass
+
+if __name__ == "__main__":
+    main()
